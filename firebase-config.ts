a31ae2df385991d575e52b900556b0a5309a6e8a
moonformeli <mgyang95@gmail.com>
2024-10-06 09:33:32 +0900

    fix: gitignore 수정

---
+++ firebase-config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCDEI0ddzqiVhZDMEyJtP2hNCxe46lacB0',
+  authDomain: 'fcm-test-a4d5e.firebaseapp.com',
+  projectId: 'fcm-test-a4d5e',
+  storageBucket: 'fcm-test-a4d5e.appspot.com',
+  messagingSenderId: '421314617430',
+  appId: '1:421314617430:web:7a2e574cbf98c4a7351bf7',
+};
